15a15cb74542d6b9705eacd65cb3e18ecb667279
talryan <nataliamryan@gmail.com>
2020-12-16 10:58:12 -0800

    Sign Up fixed.

--- doc-it/app/controllers/user_controller.rb
+++ doc-it/app/controllers/user_controller.rb
@@ -5,7 +5,6 @@ class UserController < ApplicationController
     end
 
     get '/signup' do
-        redirect_if_not_logged_in
         erb :"users/new"
     end
 
@@ -43,13 +42,6 @@ class UserController < ApplicationController
         end
     end
 
-    # post '/login' do 
-    #     @user = User.find_by(username: params[:username])
-    #     if @user && @user.authenticate(params[:password])
-    #      session[:user_id] = @user.id
-    #      redirect "/index"
-    #     end
-    # end
 
     get "/users/:id" do
         redirect_if_not_logged_in
